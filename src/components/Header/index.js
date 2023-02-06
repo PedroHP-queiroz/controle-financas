78414deb8d944bc46ccd3e801d11e46c9214b1c0
PEDRO HENRIQUE PEREIRA DE QUEIROZ <101682986+PedroHP-queiroz@users.noreply.github.com>
2023-02-06 12:18:07 -0300

    .

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+    return (
+        <C.Container>
+            <C.Header>
+                <a href="https://github.com/PedroHP-queiroz" target="_blank" rel="noreferrer"><C.Title>Controle Financeiro:</C.Title></a>
+            </C.Header>
+        </C.Container>
+    );
+};
+
+export default Header;
