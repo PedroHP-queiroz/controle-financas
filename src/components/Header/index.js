7cab3cb50325478201edb769277927fbf7e32994
PEDRO HENRIQUE PEREIRA DE QUEIROZ <101682986+PedroHP-queiroz@users.noreply.github.com>
2023-02-22 05:07:23 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <C.Container>
             <C.Header>
-                <a href="https://github.com/PedroHP-queiroz" target="_blank" rel="noreferrer"><C.Title>Controle Financeiro:</C.Title></a>
+                <a href="https://github.com/PedroHP-queiroz" target="_blank" rel="noreferrer"><C.Title>Controle Financeiro V1.0:</C.Title></a>
             </C.Header>
         </C.Container>
     );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -13,4 +13,4 @@ export const Header = styled.h1``;
 export const Title = styled.div`
     padding-top: 20px;
     color: #fff;
-`;
+    `;
